67d34cee1c9502d368064a0180a15230c8f3e66a
cesardarinel <cesardarinelortiz@gmail.com>
2013-05-03 12:38:34 -0400

    Ejercicio completo

---
+++ ClienteFroms/ClienteFroms/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ClienteFroms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConectar = new System.Windows.Forms.Button();
+            this.btEnviar = new System.Windows.Forms.Button();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.tbEnviar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btConectar
+            // 
+            this.btConectar.Location = new System.Drawing.Point(197, 29);
+            this.btConectar.Name = "btConectar";
+            this.btConectar.Size = new System.Drawing.Size(75, 23);
+            this.btConectar.TabIndex = 0;
+            this.btConectar.Text = "Conectar";
+            this.btConectar.UseVisualStyleBackColor = true;
+            this.btConectar.Click += new System.EventHandler(this.btConectar_Click);
+            // 
+            // btEnviar
+            // 
+            this.btEnviar.Location = new System.Drawing.Point(200, 194);
+            this.btEnviar.Name = "btEnviar";
+            this.btEnviar.Size = new System.Drawing.Size(75, 55);
+            this.btEnviar.TabIndex = 1;
+            this.btEnviar.Text = "Enviar";
+            this.btEnviar.UseVisualStyleBackColor = true;
+            this.btEnviar.Click += new System.EventHandler(this.btEnviar_Click);
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(16, 29);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(175, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbChat
+            // 
+            this.tbChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbChat.Location = new System.Drawing.Point(12, 55);
+            this.tbChat.Multiline = true;
+            this.tbChat.Name = "tbChat";
+            this.tbChat.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbChat.Size = new System.Drawing.Size(260, 133);
+            this.tbChat.TabIndex = 3;
+            // 
+            // tbEnviar
+            // 
+            this.tbEnviar.Location = new System.Drawing.Point(16, 229);
+            this.tbEnviar.Name = "tbEnviar";
+            this.tbEnviar.Size = new System.Drawing.Size(175, 20);
+            this.tbEnviar.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbEnviar);
+            this.Controls.Add(this.tbChat);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.btEnviar);
+            this.Controls.Add(this.btConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConectar;
+        private System.Windows.Forms.Button btEnviar;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbChat;
+        private System.Windows.Forms.TextBox tbEnviar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ClienteFroms/ClienteFroms/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ClienteFroms
+{
+    public partial class Form1 : Form
+    {
+        TcpClient Cliente;
+        NetworkStream StreamCliente;
+        string mensaje;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Cliente = new TcpClient("127.0.0.1", 8888);
+                StreamCliente = Cliente.GetStream();
+                byte[] data = Encoding.ASCII.GetBytes(tbNombre.Text);
+                StreamCliente.Write(data, 0, data.Length);
+                StreamCliente.Flush();
+               mensaje = "Conectando Con el Servidor";
+               Mensaje_REcivido();
+               Thread Hilo = new Thread(Recivir_Mensaje);
+               Hilo.Start();
+
+            }
+            catch 
+            {
+                MessageBox.Show("Error Al conectar");
+            }
+
+        }
+
+        private void btEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamCliente = Cliente.GetStream();
+                byte[] data = Encoding.ASCII.GetBytes(tbEnviar.Text);
+                StreamCliente.Write(data, 0, data.Length);
+                StreamCliente.Flush();
+            }
+            catch
+            {
+                MessageBox.Show("No pudo enviar el msj");
+            }
+        }
+        public void Mensaje_REcivido()
+        {
+            if (InvokeRequired)
+                Invoke(new MethodInvoker(Mensaje_REcivido));
+            else
+                tbChat.Text += "\n  =>>" + mensaje;
+        }
+
+        private void Recivir_Mensaje()
+        { 
+            while(true)
+            {
+                StreamCliente = Cliente.GetStream();
+                byte[] bit = new byte[140];
+                StreamCliente.Read(bit, 0, bit.Length);
+                mensaje = Encoding.ASCII.GetString(bit);
+                Mensaje_REcivido();
+
+            }
+        
+        }
+    }
+}
